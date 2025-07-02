e7a88205a97eaa7dc0f8501a698bad9f99433e75
Rahul Menon <rahul.menon85280@gmail.com>
2025-07-02 02:04:17 -0500

    Moved things around a bit

--- client/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/client.AssemblyInfo.cs
+++ client/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/client.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+176c48ad5572b9a46c4d41b0bbeea573c248a461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da7ac3307c069c090ac5a1386f9a2c8b5485e141")]
 [assembly: System.Reflection.AssemblyProductAttribute("client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
